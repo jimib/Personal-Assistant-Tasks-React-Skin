c227fdb5ad0a2b90812e0ac8d376738082c8979b
jimib <jimi.bailey@gmail.com>
2018-10-05 09:51:55 +0100

    Reduced down to just the tasks required for React Skin

--- tasks/add-component.js
+++ tasks/add-component.js
@@ -5,8 +5,7 @@ const TASK_NAME = 'Add Component/Container';
 
 const COMPONENT = 'component';
 const COMPONENT_STYLESHEET = 'component-stylesheet';
-const CONTAINER = 'container';
-const DIR_CLIENT = './client';
+const DIR_CLIENT = './src';
 
 task( TASK_NAME )
 .then( (assistant, options = {}) => {
@@ -24,9 +23,7 @@ task( TASK_NAME )
 			options: {
 				items: [
 					{label : 'Component', value: COMPONENT, enabled: true},
-					{label : 'Component Stylesheet', value: COMPONENT_STYLESHEET, enabled: true},
-					{type : 'seperator'},
-					{label : 'Container', value: CONTAINER, enabled: true}
+					{label : 'Component Stylesheet', value: COMPONENT_STYLESHEET, enabled: true}
 				]
 			}
 		}
@@ -40,9 +37,6 @@ task( TASK_NAME )
 			_.includes( includes, COMPONENT ) ? () => assistant.editCodeBlock( `${DIR_CLIENT}/components/${name}Component.js`, 'RENDER', {language:'jsx'} ) : null,
 			_.includes( includes, COMPONENT_STYLESHEET ) ? () => assistant.template( `${DIR_CLIENT}/components/css/${name}Component.styl`, '../templates/BasicComponentStyle.styl', options ) : null,
 			_.includes( includes, COMPONENT_STYLESHEET ) ? () => assistant.editCode( `${DIR_CLIENT}/components/css/${name}Component.styl`, {language:'stylus'} ) : null,
-			_.includes( includes, CONTAINER ) ? () => assistant.template( `${DIR_CLIENT}/containers/${name}Container.js`, '../templates/BasicContainer.js', options ) : null,
-			_.includes( includes, CONTAINER ) ? () => assistant.editCodeBlock( `${DIR_CLIENT}/containers/${name}Container.js`, 'MAP_PROPS', {language:'javascript'} ) : null,
-			_.includes( includes, CONTAINER ) ? () => assistant.editCodeBlock( `${DIR_CLIENT}/containers/${name}Container.js`, 'MAP_DISPATCH', {language:'javascript'} ) : null
 		]), handler => handler() );
 	})
 } );
